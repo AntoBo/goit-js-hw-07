952f2f9cf5dc6f81bb89f38b8e36b49701d14d6f
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-04 16:40:49 +0200

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems));
+galleryEl.addEventListener('click', onGalleryClick);
+let basicLightboxinstance;
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join('');
+}
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+
+  basicLightboxinstance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`);
+
+  basicLightboxinstance.show();
+
+  window.addEventListener('keydown', onEscPress);
+}
+
+function onEscPress(event) {
+  if (event.code !== 'Escape') return;
+
+  basicLightboxinstance.close();
+  window.removeEventListener('keydown', onEscPress);
+}
