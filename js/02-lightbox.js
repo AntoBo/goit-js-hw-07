752a831c8cfe5df1e90bc1d7bd91f0cbdd7863e6
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-04 17:54:09 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('afterbegin', createGalleryMarkup(galleryItems));
+// galleryEl.addEventListener('click', onGalleryClick);
+// let basicLightboxinstance;
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="l${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join('');
+}
